2fc4f5e1e84912396c0dad2a78eb620ac05453bb
sophiawillingham <sophiwilli@gmail.com>
2025-08-28 11:48:56 -0500

    Added new activity for repair order and a header text

---
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -0,0 +1,24 @@
+package com.example.repairorder;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RepairOrderActivity extends AppCompatActivity { //parent child inheritance the base class for an activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //the momment the app runs this method will run
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_repair_order);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
